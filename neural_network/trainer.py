b901c84c1291a95c44844e049bba9feb943b690a
Darmstadti-um <106447442+Darmstadti-um@users.noreply.github.com>
2024-06-16 11:10:30 +0300

    Update and rename trainer to trainer.py

---
+++ neural_network/trainer.py
@@ -0,0 +1,15 @@
+import torch
+from torch import optim
+from torch import nn
+from utils import *
+import os
+
+import datetime
+import time
+from tensorboardX import SummaryWriter
+import matplotlib.pyplot as plt
+import numpy as np
+import params
+from tqdm import tqdm
+
+
